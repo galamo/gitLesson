376494d90f6659b04a7ce66a11523db4aacf0d01
Tomer Eyal <tomereyal93@gmail.com>
2020-06-16 14:37:36 +0300

    added new list on index.html and a new function on index.js

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-(function (){
-    console.log("this function works")
-    const a = 1;
-    console.log(a)
+(function () {
+  console.log("this function works");
+  const a = 1;
+  console.log(a);
+})()(
+  //This is my function
 
-    
-})()
+  function () {
+    console.log("this is Tomer's function");
+    const b = 2;
+    console.log(b);
+  }
+)();
