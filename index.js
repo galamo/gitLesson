eccca4a6fea4a10142fce0bdd27daf781c7383b7
ShaharShi <Shaharshitrit01@gmail.com>
2020-06-18 08:37:25 +0300

    Added prompts to put some text in li elements

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 (function (){
-    console.log("this function works")
-    const a = 1;
-    console.log(a)
+    let firstName = prompt('Enter worker number 1');
+    let secoundName = prompt('Enter worker number 2');
+    let thirdName = prompt('Enter worker number 3');
+    let forthName = prompt('Enter worker number 4');
+    let fifthName = prompt('Enter worker number 5');
 
+    document.getElementById('firstLi').innerText = firstName;
+    document.getElementById('secondLi').innerText = secoundName;
+    document.getElementById('thirdLi').innerText = thirdName;
+    document.getElementById('fourthLi').innerText = forthName;
+    document.getElementById('fifthLi').innerText = fifthName;
     
 })()
+
